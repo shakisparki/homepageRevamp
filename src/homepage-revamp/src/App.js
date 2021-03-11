66b78fce223c04dfed64aa9858acbd0e7c0d8a51
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-03-11 17:19:54 +0000

    use react router for header

--- src/homepage-revamp/src/App.js
+++ src/homepage-revamp/src/App.js
@@ -1,39 +1,24 @@
 
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import Header from "./components/header";
 import ControlPanel from "./components/control";
-import { ChakraProvider } from "@chakra-ui/react"
+import {ChakraProvider} from "@chakra-ui/react";
+import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
+import {ContentView} from "./utilities"
 
-// function App() {
-//   return (
-  class App extends Component{
-    constructor(props){
-      super(props);
-      this.state = {noOfListItems: 0};
-    }
-
-    render(){
-      return(
-        <ChakraProvider>
-          <Header noOfListItems={this.state.noOfListItems}/>
-          <ControlPanel addToList={()=>this.addToList()} 
-            removeFromList={()=>this.removeFromList()}/>
-        </ChakraProvider>
-      )
-    }
-  // );
-
-  // Control functions
-  addToList() {
-    const value = this.state.noOfListItems + 1;
-    this.setState({noOfListItems : value});
-  }
-  removeFromList() {
-    if(this.state.noOfListItems > 0){
-      const value = this.state.noOfListItems - 1;
-      this.setState({noOfListItems : value});
-    }
-  }
+function App(){
+  const [noOfListItems,setListNo] = useState(0);
+  return(
+    <ChakraProvider>
+      <Router>
+        <Header noOfListItems={noOfListItems}/>
+        <ControlPanel addToList={()=>setListNo(noOfListItems + 1)} 
+          removeFromList={()=>{noOfListItems > 0 && setListNo(noOfListItems - 1)}}/>
+        <Switch>
+          <Route component={ContentView}/>
+        </Switch>
+      </Router>
+    </ChakraProvider>
+  )
 }
-
 export default App;
--- src/homepage-revamp/src/components/header.js
+++ src/homepage-revamp/src/components/header.js
@@ -7,18 +7,16 @@ import {ChevronDownIcon} from "@chakra-ui/icons"
 import { MdStar, MdAccountCircle } from "react-icons/md";
 import {FiLogIn, FiLogOut} from "react-icons/fi";
 import {IoPersonAdd} from "react-icons/io5";
+import {Link,useLocation} from "react-router-dom";
 import nav from "../nav/nav.json"
 
 function NavMenu(props){
-  const currentPage = (window.location.pathname === props.href);
   return(
-    <Menu>
-      {/* <MenuButton as="a" href={props.href} size="lg">
-        <Text fontWeight={ currentPage ? "bold" : "normal"}>{props.title}</Text> */}
-      <MenuButton size="lg" _active={{ fontWeight: 'bold' }}> {/* for demonstration */}
+    <Link to={props.href}>
+      <Text fontWeight={props.loc === props.href ? "bold" : "normal"}>
         {props.title}
-      </MenuButton>
-     </Menu>
+      </Text>
+    </Link>
   )
 }
 
@@ -33,72 +31,69 @@ function MyList(props){
   )
 }
 
-class Header extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {loggedIn : false};
-  }
+function Header(props){
+  const [show, ] = React.useState(false);
+  const [loggedIn,setLoggedIn ] = React.useState(false);
+  let location = useLocation();
 
-  render(){
-    // const [show, ] = React.useState(false);
-
-    const menudata = nav.map((data) => {
-      return (
-        <NavMenu title={data.title} href={data.action}/>
-      )
-    });
-
+  const menudata = nav.map((data) => {
     return (
-      <Flex
-        as="nav"
-        align="center"
-        justify="space-between"
-        wrap="wrap"
-        padding="1.5rem"
-        bg="white.500"
-        borderBottom="1px"
-        color="black"
-        {...this.props}
-      >
-        <LinkBox>
-          <Flex align="center" mr={5}>
-          <LinkOverlay href="/">
-            <Image src={logo}></Image>
-          </LinkOverlay>
-          </Flex>
-        </LinkBox>
+      <NavMenu title={data.title} href={data.action}
+         loc={location.pathname}/>
+    )
+  });
 
-      <Stack spacing={8}>
-        {/* Flex for the search bar and two buttons */}
-        <Flex>
-        <Box
-          display={{ sm: this.show ? "block" : "none", md: "block" }}
-          mt={{ base: 4, md: 0 }}
-        >
-        <InputGroup size="lg">
-            <Input borderRadius="1px" pr="20rem" placeholder="Search for samples"/>
-            <InputRightElement width="5rem">
-            <Button borderRadius="1px" bg="#808080" variant="solid" color="white" _hover={{ bg: "#4d4d4d" }}  size="lg">
-                Search
-            </Button>
-            </InputRightElement>
-        </InputGroup>
-      </Box>
-      <ButtonGroup spacing={4} ml="1.5rem">
-            <MyList noOfItems={this.props.noOfListItems}/>
-            <Menu>
-              <MenuButton as={Button} rightIcon={<ChevronDownIcon />} 
-                leftIcon={<Icon as={MdAccountCircle} boxSize={8}/>} 
-                borderRadius="1px" variant="outline" size="lg">
-                My Account
-              </MenuButton>
-              <MenuList>
-                <MenuItem onClick={()=>{
-                  this.setState({loggedIn:!this.state.loggedIn});
-                  alert("You have been logged " + (this.state.loggedIn ? "in" : "out"));
-                  }}>
-                {
-                    this.state.loggedIn ?
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1.5rem"
+      bg="white.500"
+      borderBottom="1px"
+      color="black"
+      {...props}
+    >
+      <LinkBox>
+        <Flex align="center" mr={5}>
+        <LinkOverlay href="/">
+          <Image src={logo}></Image>
+        </LinkOverlay>
+        </Flex>
+      </LinkBox>
+
+    <Stack spacing={8}>
+      {/* Flex for the search bar and two buttons */}
+      <Flex>
+      <Box
+        display={{ sm: show ? "block" : "none", md: "block" }}
+        mt={{ base: 4, md: 0 }}
+      >
+      <InputGroup size="lg">
+          <Input borderRadius="1px" pr="20rem" placeholder="Search for samples"/>
+          <InputRightElement width="5rem">
+          <Button borderRadius="1px" bg="#808080" variant="solid" color="white" _hover={{ bg: "#4d4d4d" }}  size="lg">
+              Search
+          </Button>
+          </InputRightElement>
+      </InputGroup>
+    </Box>
+    <ButtonGroup spacing={4} ml="1.5rem">
+          <MyList noOfItems={props.noOfListItems}/>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} 
+              leftIcon={<Icon as={MdAccountCircle} boxSize={8}/>} 
+              borderRadius="1px" variant="outline" size="lg">
+              My Account
+            </MenuButton>
+            <MenuList>
+              <MenuItem onClick={()=>{
+                setLoggedIn(!loggedIn);
+                alert("You have been logged " + (!loggedIn ? "in" : "out"));
+                }}>
+                  {
+                    !loggedIn ?
                     <>
                       <Icon as={FiLogIn} mr="10px"/> 
                       <span>Login</span>
@@ -109,34 +104,32 @@ class Header extends React.Component{
                       <span>Logout</span>
                     </>
                   }
-                  
-                </MenuItem>
-                <MenuItem>
-                  <Icon as={IoPersonAdd} mr="10px"/> 
-                    <span>Register</span>
-                </MenuItem>
-              </MenuList>
-            </Menu>
-          </ButtonGroup>
+                
+              </MenuItem>
+              <MenuItem>
+                <Icon as={IoPersonAdd} mr="10px"/> 
+                  <span>Register</span>
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </ButtonGroup>
 
-      </Flex>
+    </Flex>
 
-      <Box
-          display={{ sm: this.show ? "block" : "none", md: "block" }}
-          mt={{ base: 4, md: 0 }}>
+    <Box
+        display={{ sm: show ? "block" : "none", md: "block" }}
+        mt={{ base: 4, md: 0 }}>
 
-          <ButtonGroup spacing={20} role="navigation">
-            {menudata}
-          </ButtonGroup>
-        
-        </Box>
-      </Stack>
+        <ButtonGroup spacing={20} role="navigation">
+          {menudata}
+        </ButtonGroup>
+      </Box>
+    </Stack>
 
-      </Flex>
+    </Flex>
 
 
-    );
-  };
-}
+  );
+};
 
 export default Header;
